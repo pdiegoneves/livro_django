6204cd340093735ac0b90214d85edee9006b4ad6
Diego Neves <pdcassiano@gmail.com>
2023-06-20 12:14:10 -0300

    fieldsets

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -5,14 +5,32 @@ class ProfileAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     # list_display = ('user', 'role', 'birthday', 'specialtiesList','addressesList',)
     list_display = ('user', 'role', 'birthday',)
+    list_display = ('user', 'birth',)
+
+    def birth(self, obj):
+        if obj.birthday:
+            return obj.birthday.strftime("%d/%m/%Y")
+
     empty_value_display = 'Vazio'
-    list_display_links = ('user', 'role')
+    # list_display_links = ('user', 'role')
     list_filter = ('user__is_active', )
-    fields = ('user', ('role',), 'image', 'birthday', 'specialties', 'addresses',)
+    # fields = ('user', ('role',), 'image', 'birthday', 'specialties', 'addresses',)
     exclude = ('favorites', 'created_at', 'updated_at')
     readonly_fields = ('user',)
     search_fields = ('user__username',)
     list_filter  = ('user__is_active', 'role')
+
+    fieldsets = (
+        ('Usuário', 
+            {'fields': ('user', 'birthday', 'image')
+        }),
+        ('Função', 
+            {'fields': ('role',)
+        }),
+        ('Extras', 
+            {'fields': ('specialties', 'addresses')
+        }),
+    )
 
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
